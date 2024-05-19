5db9989f59345cc9b777b014cbdbe9583eb8829c
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-05-19 16:30:22 -0300

    Add files via upload

---
+++ recursividade/Lista de Atividades/ex4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int divisao(int num, int denominador) {
+    if (num < denominador) {
+        return num;
+    } else {
+        return divisao(num - denominador, denominador);
+    }
+}
+
+int main() {
+    int num, denominador;
+    printf("Numerador: ");
+    scanf("%d", &num);
+    printf("Denominador: ");
+    scanf("%d", &denominador);
+
+    // Verificação de divisão por zero
+    if (denominador == 0) {
+        printf("Erro: Divisão por zero não é permitida.\n");
+    } else {
+        printf("A divisão desses números é %d\n", divisao(num, denominador));
+    }
+
+    return 0;
+}
+
