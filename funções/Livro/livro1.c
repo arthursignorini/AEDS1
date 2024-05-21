8160c7bd12b0ba65fda56f60b75bca15e0b9e661
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-05-20 22:17:13 -0300

    Update livro1.c

--- funções/Livro/livro1.c
+++ funções/Livro/livro1.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+int soma(int n){
+    int sum=0;
+    for(int i=0; i<=n; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+int main()
+{
+    int n;
+    printf("Digite um numero: ");
+    scanf("%d",&n);
+    printf("A soma eh %d",soma(n));
+}
