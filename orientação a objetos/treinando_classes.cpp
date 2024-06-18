0c1054cd75c094cbcc7e62aa6f54ff6557c75057
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-06-18 19:02:25 -0300

    Update treinando_classes.cpp

--- orientação a objetos/treinando_classes.cpp
+++ orientação a objetos/treinando_classes.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
 
+using namespace std;
+
+class Pessoa {
+
+private:
+
+  string nome;
+  int idade;
+
+public:
+
+  Pessoa(string nome, int idade){
+    this -> nome = nome;
+    this -> idade = idade;
+  }
+
+  string getNome(){
+    return nome;
+  }
+
+  int getIdade(){
+    return idade;
+  }
+};
+
+int main(){
+  Pessoa p1("Arthur", 18);
+  cout << p1.getNome() << endl;
+  cout << p1.getIdade() <<endl;
+
+  Pessoa p2("Mario",23);
+  cout << p2.getNome() << endl;
+  cout << p2.getIdade() <<endl;
+}
